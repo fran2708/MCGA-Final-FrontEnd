be9f9c51ec57b540cf2f2507a2bf38565a8c5154
fran2708 <francopaladini2708@gmail.com>
2024-03-04 21:21:01 -0300

    feat: added actions, reducer and types for login

---
+++ src/Redux/Login/reducer.js
@@ -0,0 +1,61 @@
+import {
+    GET_LOGIN_PENDING,
+    GET_LOGIN_SUCCESS,
+    GET_LOGIN_ERROR,
+    SET_TOKEN,
+    LOGOUT
+  } from './types'
+  
+  const initialState = {
+    isFetching: false,
+    error: '',
+    user: 
+    {
+        name: '',
+        email: '',
+        token: ''
+    },
+    token: ''
+  }
+  
+  const loginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_LOGIN_SUCCESS: 
+            return {
+            ...state,
+            isFetching: false,
+            list: state.list.map((item) => {
+                if (item._id === action.payload._id) {
+                return action.payload
+                }
+                return item
+            })
+            }
+        case GET_LOGIN_ERROR: 
+            return {
+            ...state,
+            isFetching: false,
+            error: action.payload
+            }
+        case GET_LOGIN_PENDING: 
+            return {
+            ...state,
+            isFetching: true,
+            error: initialState.error
+            }    
+        case SET_TOKEN: 
+            return {
+            ...state,
+            token: action.payload
+            }
+        case LOGOUT:
+            return {
+            ...state,
+            token: action.payload.token,
+            user: action.payload.user
+            }
+        default: return state
+    }
+  }
+  
+  export default loginReducer
