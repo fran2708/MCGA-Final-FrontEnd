fa6c29646d744757de3d3a079b8ba02c8dcf8e3d
fran2708 <francopaladini2708@gmail.com>
2024-03-04 21:58:03 -0300

    feat: modified the store to include the login reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import productsReducer from './Products/reducer'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import productsReducer from './Products/reducer'
+import loginReducer from './Login/reducer'
 
-const rootReducer = createStore(productsReducer, applyMiddleware(thunk))
-
-const store = rootReducer
+const store = createStore(
+    combineReducers({ products: productsReducer, login: loginReducer }),
+    applyMiddleware(thunk)
+)
 
 export default store
