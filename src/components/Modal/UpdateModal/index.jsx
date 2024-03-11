8942f03bfce08f27662936be52ee11ed0b5b35c7
fran2708 <francopaladini2708@gmail.com>
2024-03-10 22:14:55 -0300

    fix: changed the way the modal works. now imports add or edit modal according to users action

---
+++ src/components/Modal/UpdateModal/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index () {
+    return (
+        <div>Update Modal</div>
+    )
+}
+
+export default index
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,18 +1,20 @@
-import { useDispatch } from 'react-redux'
-import { addProductLoading } from '../../Redux/Products/actions'
-import styles from './modal.module.css'
 import { useState } from 'react'
-import { addProduct, editProduct } from '../../Redux/Products/thunks'
+import { useDispatch } from 'react-redux'
+
+import { addProduct, updateProduct } from '../../Redux/Products/thunks'
+import { actionTypes } from '../../models/actionTypes'
+
+import styles from './modal.module.css'
 
 const Modal = ({ isOpen, handleClose, action, product }) => {
+    const dispatch = useDispatch()
+    const [isConfirming, setIsConfirming] = useState(false)
     const [newProductData, setNewProductData] = useState({
         name: '',
         price: '',
         stock: '',
         description: ''
     })
-
-    const dispatch = useDispatch()
 
     const handleCancelButtonClick = () => {
         handleClose()
@@ -22,6 +24,7 @@ const Modal = ({ isOpen, handleClose, action, product }) => {
             stock: '',
             description: ''
         })
+        setIsConfirming(false)
     }
     const handleChange = (event) => {
         setNewProductData((prevData) => ({
@@ -31,24 +34,53 @@ const Modal = ({ isOpen, handleClose, action, product }) => {
     }
 
     const handleSubmit = () => {
-        dispatch(editProduct(product._id, newProductData))
+        dispatch(updateProduct(product._id, newProductData))
         handleClose()
     }
+
     return (
         <div className={isOpen ? styles.modalOpen : styles.modal}>
             <div className={styles.modalContent}>
+                {/* TOP ROW */}
                 <div className={styles.topRow}>
-                    <h2>{action}</h2>
+                    <h2>{action === actionTypes.EDIT ? `Editing: ${product.name}` : 'Adding new product'}</h2>
                     <button className={styles.cancelButton} onClick={handleCancelButtonClick}>Cancel</button>
                 </div>
-                <div className={styles.middleRow}>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                    <p>{product.stock}</p>
-                    <p>{product.description}</p>
-                </div>
+                {/* MIDDLE ROW */}
+                { action === actionTypes.EDIT
+                    ? <EditModal
+                        newProductData={newProductData}
+                        setNewProductData={setNewProductData}
+                        product={product}
+                    />
+                    : <AddModal
+                        newProductData={newProductData}
+                        setNewProductData={setNewProductData}
+                    />
+                }
+                {/* BOTTOM ROW */}
                 <div className={styles.bottomRow}>
-                    <button>Save</button>
+                    { isConfirming
+                        ? <div className={styles.confirmationContainer}>
+                            <p>Do you confirm this action?</p>
+                            <button
+                                className={styles.confirmButton}
+                                type="submit"
+                                onClick={handleChange}>
+                            Confirm
+                            </button>
+                            <button
+                                className={styles.cancelConfirmButton}
+                                onClick={() => setIsConfirming(!isConfirming)} >
+                            Cancel</button>
+                        </div>
+                        : <button
+                            className={styles.saveButton}
+                            type="submit"
+                            onClick={() => setIsConfirming(!isConfirming)} >
+                            Save
+                        </button>
+                    }
                 </div>
             </div>
         </div>
